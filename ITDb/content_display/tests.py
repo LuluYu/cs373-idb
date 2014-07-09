e281cc9a733dc9184dded46620147fc00119e96d
Juan P. Mata <jpmata1@gmail.com>
2014-07-09 15:36:16 -0500

    added cape town info

--- ITDb/content_display/tests.py
+++ ITDb/content_display/tests.py
@@ -6,11 +6,11 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from content_display.models import Cities Activities Languages
 
+class CityTest(TestCase) :
+    def test_c1(self):
+        c1 = Cities.objects.create(name = 'Li Jiang')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        c = Cities(name = 'Li Jiang', size = '8,193 sq mi', population = '1,244,769')
+
